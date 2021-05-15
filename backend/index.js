d0283aa72f9eb4a38a126c37e29a94d165b25ee9
blopez-dev <pjgl.frontend@gmail.com>
2021-05-15 19:17:03 +0200

    Terminate Boilerplate

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const debug = require('debug')('server');
-const chalk = require('chalk');
-const morgan = require('morgan');
-require('dotenv').config();
 
-const server = express();
-server.use(cors());
-server.use(express.json());
-server.use(morgan('dev'));
-
-server.listen('2025', () => debug(`Server is running in ${chalk.yellow('localhost:2025')}`));
